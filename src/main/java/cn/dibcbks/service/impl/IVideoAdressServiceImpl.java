ca18ae1ec86646eb4a17f2343d9739f3f957a1c5
wupeilong <gm1992@163.com>
2020-04-09 16:06:05 +0800

    1111111

--- src/main/java/cn/dibcbks/service/impl/IVideoAdressServiceImpl.java
+++ src/main/java/cn/dibcbks/service/impl/IVideoAdressServiceImpl.java
@@ -26,7 +26,6 @@ public class IVideoAdressServiceImpl implements IVideoAddressService {
 		 * 根据企业id查询视频流地址
 		 */
 		List<VideoAddress> list = videoAddressMapper.SelectVideoAddressById(id);
-		System.out.println("http://localhost:8080/DIBC_BKS/wap_video/wap_videodetal:"+id + list);
 		try {
 			map.addAttribute("videoAddressList", list);
 		} catch (Exception e) {
