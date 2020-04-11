14b68ecec662a64c0fea43f0e70fde4d31155e91
wupeilong <gm1992@163.com>
2020-04-11 16:17:54 +0800

    1111111111111111111111

--- src/main/java/cn/dibcbks/service/impl/IDepartmentServiceImpl.java
+++ src/main/java/cn/dibcbks/service/impl/IDepartmentServiceImpl.java
@@ -1,12 +1,13 @@
 package cn.dibcbks.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import cn.dibcbks.entity.Department;
 import cn.dibcbks.entity.Unit;
 import cn.dibcbks.mapper.DepartmentMapper;
 import cn.dibcbks.service.IDepartmentService;
 
+@Service
 public class IDepartmentServiceImpl implements IDepartmentService {
 	@Autowired
 	private DepartmentMapper departmentMapper;
@@ -20,19 +21,28 @@ public class IDepartmentServiceImpl implements IDepartmentService {
 		department.setDepartmentHead(unit.getLegalPerson());//负责人
 		department.setDepartmentParentId(0);//最上级
 		department.setDepartmentType(6);//主体
-		department.setAuthorizationId(3);//三级权限
+		department.setAuthorizationId(4);//三级权限
 		departmentMapper.insert(department);
+		Integer departmentParentId = department.getDepartmentId();//企业部门ID
 		
+		department = new Department();
+		department.setUnitId(unit.getUnitId());//企业Id
 		department.setDepartmentName("负责人");
 		department.setDepartmentHead(unit.getLegalPerson());//负责人
-		department.setDepartmentParentId(department.getDepartmentId());//父级部门ID
+		department.setDepartmentParentId(departmentParentId);//父级部门ID
+		department.setDepartmentType(6);//主体
+		department.setAuthorizationId(5);//五级权限
 		departmentMapper.insert(department);
 		
 		Integer legalPersonDepartmentId = department.getDepartmentId();//负责人部门ID
 		
+		department = new Department();
+		department.setUnitId(unit.getUnitId());//企业Id
 		department.setDepartmentName("从业人员");
 		department.setDepartmentHead(unit.getLegalPerson());//负责人
-		department.setDepartmentParentId(department.getDepartmentParentId());//父级部门ID
+		department.setDepartmentParentId(departmentParentId);//父级部门ID
+		department.setDepartmentType(6);//主体
+		department.setAuthorizationId(5);//五级权限
 		departmentMapper.insert(department);
 		
 		return legalPersonDepartmentId;
